7941b659885000f99dd54b151296923cfa43a991
Pokrishk <anna.kononova41@gmail.com>
2024-03-28 12:48:35 +0300

    Добавьте файлы проекта.

---
+++ z/z.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+class Auto {
+public:
+    string marka;
+    string model;
+    int year;
+    string color;
+    int probeg;
+    Auto(string Marka, string Model, int Year, string Color, int Probeg) {
+        this->marka = Marka;
+        this->model = Model;
+        this->year = Year;
+        this->color = Color;
+        this->probeg = Probeg;
+    }
+    void info() {
+        cout << "Марка: " << marka << endl;
+        cout << "Модель: " << model << endl;
+        cout << "Год выпуска: " << year << endl;
+        cout << "Цвет: " << color << endl;
+        cout << "Пробег: " << probeg << endl;
+    }
+    void plprobeg() {
+        cout << "Пробег увеличен с " << probeg << " на 10 км. \nИтоговый пробег: " << probeg + 10 << endl;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    Auto a("", "", 1010, "", 100);
+    a.info();
+    a.plprobeg();
+}
